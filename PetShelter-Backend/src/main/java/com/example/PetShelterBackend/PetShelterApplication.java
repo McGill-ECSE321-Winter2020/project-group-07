54ca69a875cd070072c07d2805dce556cd558557
kdassharma <kaustav.dassharma@mail.mcgill.ca>
2020-02-05 23:58:24 -0500

    Added spring backend

---
+++ PetShelter-Backend/src/main/java/com/example/PetShelterBackend/PetShelterApplication.java
@@ -0,0 +1,13 @@
+package com.example.PetShelterBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetShelterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetShelterApplication.class, args);
+	}
+
+}
---
+++ PetShelter-Backend/src/test/java/com/example/PetShelterBackend/PetShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PetShelterBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
