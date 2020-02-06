f1629ca3effb590622f2dbee6f3d4a4578e91439
rbehal <rahulbehal01@hotmail.com>
2020-02-06 16:09:15 -0500

    Initial commit of the backend application

---
+++ PetShelter-Backend/src/test/java/ca/mcgill/ecse321/petshelter/PetShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.petshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
