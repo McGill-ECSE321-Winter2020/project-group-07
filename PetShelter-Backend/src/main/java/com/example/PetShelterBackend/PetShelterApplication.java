3a617080ab644d2205befc47bd4fad382b330f7f
kdassharma <kaustav.dassharma@mail.mcgill.ca>
2020-02-06 00:06:56 -0500

    Added test

--- PetShelter-Backend/src/main/java/com/example/PetShelterBackend/PetShelterApplication.java
+++ PetShelter-Backend/src/main/java/com/example/PetShelterBackend/PetShelterApplication.java
@@ -1,13 +1,22 @@
 package com.example.PetShelterBackend;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RestController
 @SpringBootApplication
 public class PetShelterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PetShelterApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(PetShelterApplication.class, args);
+  }
+
+  @RequestMapping("/")
+  public String greeting(){
+    return "Hello world!";
+  }
 
 }
+
