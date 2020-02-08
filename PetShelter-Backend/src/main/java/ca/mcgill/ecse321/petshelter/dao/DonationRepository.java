70b7d7feeae7500688da1f2e55527015028b890e
Alexander Legouverneur <alex.legouve@gmail.com>
2020-02-08 17:57:08 -0500

    Completed the DonationRepository class

--- PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/dao/DonationRepository.java
+++ PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/dao/DonationRepository.java
@@ -4,8 +4,8 @@ import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.petshelter.model.Donation;
 
-public interface DonationRepository extends CrudRepository<Donation, String>{
+public interface DonationRepository extends CrudRepository<Donation, Integer>{
 
-	Donation findDonationByName(String name);
+	Donation findDonationById(Integer Id);
 
 }
