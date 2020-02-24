420ebbda4f0a1cb418b132a59e152f98b6878730
rbehal <rahulbehal01@hotmail.com>
2020-02-24 15:50:51 -0500

    Added testing dependencies to gradle build and created TestPetShelterService

---
+++ PetShelter-Backend/src/test/java/ca/mcgill/ecse321/petshelter/service/TestPetShelterService.java
@@ -0,0 +1,10 @@
+package ca.mcgill.ecse321.petshelter.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+public class TestPetShelterService {
+
+}
