428d53227c74889c0c7eed7f03616ffd7c73e4bd
mohamed-youssef-bou <mohamedyoussefbouaouina@gmail.com>
2020-02-25 19:13:42 -0500

    added ApplicationServiceTests class

---
+++ PetShelter-Backend/src/test/java/ca/mcgill/ecse321/petshelter/service/ApplicationServiceTests.java
@@ -0,0 +1,43 @@
+package ca.mcgill.ecse321.petshelter.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.lenient;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+
+import ca.mcgill.ecse321.petshelter.dao.ApplicationRepository;
+
+
+@ExtendWith(MockitoExtension.class)
+public class ApplicationServiceTests {
+	
+	@Mock
+	private ApplicationRepository applicationDAO;
+	
+	@InjectMocks
+	private PetShelterService service;
+	
+	@BeforeEach
+	public void setMockOutput() {
+		//TODO: add mock for applicationDAO.findById()
+				
+		// Whenever anything is saved, just return the parameter object
+		Answer<?> returnParameterAsAnswer = (InvocationOnMock invocation) -> {
+			return invocation.getArgument(0);
+		};
+		
+		//TODO: add mock for applicationDAO.save()  
+	}
+}
