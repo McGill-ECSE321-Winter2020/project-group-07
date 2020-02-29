5c4c6e78d6ed5c7bb6705185c91639558d3ae8ef
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2020-02-28 22:00:57 -0500

    small edits on tests for comment and posting

--- PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/service/PetShelterService.java
+++ PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/service/PetShelterService.java
@@ -412,11 +412,6 @@ public class PetShelterService {
 		return comments;
 	}
 	
-//	@Transactional
-//	public Comment deleteComment(Comment comment) {
-//		
-//		return null;
-//	}
 
 	@Transactional
 	public List<Posting> getOpenPostings(){
